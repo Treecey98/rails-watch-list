04088b591c18d5e03b7ff8d64bf93820911373c2
Edward Treece <edtreece@gmail.com>
2022-08-25 16:59:08 +0100

    Basic structure of Watch List

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,16 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'lists#index'
+
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
+
+# get "lists", to: "lists#index"
+
+# get "lists/new", to: "lists#new", as: :new_list
+# post "lists", to: "lists#create"
+
+# get "lists/:id", to: "lists#show", as: :list
